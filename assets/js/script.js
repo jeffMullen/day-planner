22ff9f6633a3a17b09759276a7f0fb9bdf7c63d7
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-01 13:18:18 -0400

    moment.js tracking which hour it is

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,44 @@
 var currentDay = moment().format('dddd, MMMM Do');
 $('#currentDay').text(currentDay);
 
-// Timeblocks color coded for past, present, and future
+// || Storing current hour for color display
+var currentHour = moment().format('H');
+console.log(currentHour);
+
+// Timeblocks color coded for past, present, and future ******
+
+    // Moment.js to track which hour it is
+
+    // Variable for each large div
+
+    // Array for all time div variables
+
+    // Placeholder index that changes each time the hour changes moment.js
+
+        // Event listener on change of moment.js
+
+    // If indexes are earlier in the array - past
+
+    //  If current index - present
+
+    //  If indexes are later in array - future
+
+    // event listener on whole container
+
+
+// WHEN I click into a timeblock *******
+// THEN I can enter an event ********
+
+    // Text area in each large div
+
+
+// WHEN I click the save button for that timeblock *********
+// THEN the text for that event is saved in local storage ********
+
+    // Button on last div of each row
+
+        //  Event listener click on container targeting buttons
+
+    //  On click string is saved to local storage
+
+    //  On load text fields are populated from local storage
