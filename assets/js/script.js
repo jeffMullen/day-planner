ffe801b52f37618608183f4c8092ec1b87631b3c
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-03 14:25:26 -0400

    adding comments and deleting console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,7 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
 var planner = $('#planner');
 var entry = $('#entry');
-console.log(entry);
-console.log(planner);
 
+// || Each Timeblock
 var nine = $('#9');
 var ten = $('#10');
 var eleven = $('#11');
@@ -28,13 +14,11 @@ var seventeen = $('#17');
 
 var time = [nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen];
 
-
-
 // || Moment.js to display time
 var currentDay = moment().format('dddd, MMMM Do');
 $('#currentDay').text(currentDay);
 
-// Timeblocks color coded for past, present, and future ******
+// || Timeblocks color coded for past, present, and future ******
 var currentHour = parseInt(moment().format('H'));
 
 for (var i = 0; i < time.length; i++) {
@@ -48,75 +32,24 @@ for (var i = 0; i < time.length; i++) {
         time[i].addClass('future');
     }
 }
-
-// || Storing current hour for color display
-console.log(typeof currentHour);
-console.log(currentHour);
-
-currentHour++;
-console.log(currentHour);
-
-// Variable for each large div
-
-// Array for all time div variables
-
-// Placeholder index that changes each time the hour changes moment.js
-
-// Event listener on change of moment.js
-
-// If indexes are earlier in the array - past
-
-//  If current index - present
-
-//  If indexes are later in array - future
-
-
-
-
+// || Specific save button
 var buttonClicked;
-// event listener on whole container
+// || Event listener on whole container
 planner.on('click', 'button', function (event) {
     buttonClicked = this;
-    console.log(buttonClicked);
-    console.log('Click!');
     saveEntry();
 })
 
 function saveEntry(event) {
-    // choose textarea associated with button
-    console.log($(buttonClicked).parent().siblings().eq(1).children().children().children());
-
+    // || Choose textarea associated with button and save to local storage
     var textarea = $(buttonClicked).parent().siblings().eq(1).children().children().children();
     var inputId = textarea.attr('id');
-    // var inputValue = textarea.val();
-    // console.log(inputId, inputId);
-    // console.log(inputId, textarea);
+
     localStorage.setItem(inputId, textarea.val());
-    // console.log($(buttonClicked).parent().siblings().eq(1).children().children().children().val()
-    // );
-    // JSON.stringify()
-    // save that text area's content in local storage
-
-
 }
 
+// || Loading events to the planner
 $('textarea').each(function () {
     localStorage.getItem($(this).attr('id'));
-    console.log(localStorage.getItem($(this).attr('id'))
-    );
     $(this).val(localStorage.getItem($(this).attr('id')));
 });
-
-// WHEN I click into a timeblock *******
-// THEN I can enter an event ********
-
-// WHEN I click the save button for that timeblock *********
-// THEN the text for that event is saved in local storage ********
-
-// Button on last div of each row
-
-//  Event listener click on container targeting buttons
-
-//  On click string is saved to local storage
-
-//  On load text fields are populated from local storage
